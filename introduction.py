a2f90870d79b014ccaabdf4572c5112537605f8d
Tryphine <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2024-01-18 13:47:54 +0300

    Update introduction.py

--- introduction.py
+++ introduction.py
@@ -2,3 +2,6 @@
 import pandas as pd
 
 # Pandas has 2 main data structures (series and dataframes)
+
+# pandas Series* one-dimensional labeled array
+ser = pd.Series(data=[100, 200, 300, 400, 500], index=['tom', 'bob', 'nancy', 'dan', 'eric'])
